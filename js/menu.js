f5d37594c4c01a8edbe65a66a0357cf2da23943b
kevin_Ale <112594520+kevinAleFunes@users.noreply.github.com>
2023-10-23 20:16:51 -0300

    Cambios en la paguina principal
    En sus respectivos estilos, código, un menú e imágenes que luego vamos a usar.

--- js/menu.js
+++ js/menu.js
@@ -1,18 +1,18 @@
-const menu_uno = document.querySelector('.menu_uno')
-const menu = document.querySelector('.menu-navegacion')
+const menu_uno = document.querySelector('.menu_uno');
+const menu = document.querySelector('.menu-navegacion');
 
-console.log(menu)
-console.log(menu_uno)
+console.log(menu);
+console.log(menu_uno);
 
 
 menu_uno.addEventListener('click', ()=>{
-    menu.classList.toggle('spread')
+    menu.classList.toggle('spread');
 })
 
 
 window.addEventListener('click', e=>{
     if(menu.classList.contains('spread')
         && e.target != menu && e.target != menu_uno){
-            menu.classList.toggle('spread')
+            menu.classList.toggle('spread');
     }
 })
